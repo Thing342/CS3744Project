a2d9f51e5fab883f28f48cbb06812783d5a8cf5c
Thing342 <wesjordansemailaddress@gmail.com>
2018-03-27 16:16:18 -0400

    sample data

--- db_init.sql
+++ db_init.sql
@@ -58,7 +58,7 @@ CREATE TABLE UnitEvent
   id INT PRIMARY KEY AUTO_INCREMENT,
   unitID INT NOT NULL ,
   eventName VARCHAR(64) NOT NULL,
-  type INT NOT NULL ,
+  type VARCHAR(32) NOT NULL ,
   date DATE NOT NULL ,
   description TEXT NOT NULL ,
   locationName TEXT NOT NULL ,
@@ -69,3 +69,24 @@ CREATE TABLE UnitEvent
     ON DELETE CASCADE ON UPDATE CASCADE
 );
 ALTER TABLE UnitEvent COMMENT = 'An event that occurred to some Unit along the campaign. Can be a battle, an operation, a diary entry, etc.';
+
+/** Add Sample Data **/
+
+/** Username: adminuser; Password: mypassword **/
+INSERT INTO fantasticfour_p4.User (userId, username, pword_hash, email) VALUES (0, 'adminuser', '$2y$10$Hj1xEDJR.vr3KOPxi/iXC.et92qXsgyyb3hmM/NaAv2acz/SVxBwK', 'sample@example.com');
+
+INSERT INTO fantasticfour_p4.Unit (id, name) VALUES (0, 'Assault Gun Platoon');
+INSERT INTO fantasticfour_p4.Unit (id, name) VALUES (0, 'Reconaissance Platoon');
+
+INSERT INTO fantasticfour_p4.Person (id, unitID, rank, firstname, lastname) VALUES (0, 1, 'Pfc', 'Lawrence', 'Clark');
+INSERT INTO fantasticfour_p4.Person (id, unitID, rank, firstname, lastname) VALUES (0, 1, 'Pvt', 'Chester', 'Harej');
+INSERT INTO fantasticfour_p4.Person (id, unitID, rank, firstname, lastname) VALUES (0, 1, 'Pfc', 'Vito', 'Mikalauski');
+INSERT INTO fantasticfour_p4.Person (id, unitID, rank, firstname, lastname) VALUES (0, 2, 'Cpl', 'Harvey', 'Keller');
+INSERT INTO fantasticfour_p4.Person (id, unitID, rank, firstname, lastname) VALUES (0, 2, 'Pvt', 'Jessie', 'Staggs');
+
+INSERT INTO fantasticfour_p4.UnitEvent (id, unitID, eventName, type, date, description, locationName, latitude, longitude) VALUES
+  (0, 1, 'Sample Battle #1', 'BATTLE', '1944-12-10', 'Lorem ipsum dolor sit amet', 'Somewhere in France', 46.820675, 2.375450);
+INSERT INTO fantasticfour_p4.UnitEvent (id, unitID, eventName, type, date, description, locationName, latitude, longitude) VALUES
+  (0, 1, 'Sample Battle #2', 'BATTLE', '1945-01-12', 'Lorem ipsum dolor sit amet', 'Somewhere in France', 46.820615, 2.375450);
+INSERT INTO fantasticfour_p4.UnitEvent (id, unitID, eventName, type, date, description, locationName, latitude, longitude) VALUES
+  (0, 1, 'Sample Battle #3', 'BATTLE', '1945-01-31', 'Lorem ipsum dolor sit amet', 'Somewhere in Germany', 46.820115, 2.375321);
