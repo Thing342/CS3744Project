83d747c889188ed954801d20199edd4ee97398bb
Thing342 <wesjordansemailaddress@gmail.com>
2018-03-29 13:34:40 -0400

    added barebones company add/edit functionality; not particularly pretty but gets most of the job done

--- app/controllers/CompanyController.php
+++ app/controllers/CompanyController.php
@@ -28,6 +28,16 @@ class CompanyController extends BaseController
     public function routes(): array
     {
         return [
+            self::route("POST", "/add", 'companyAdd'),
+
+            self::route("POST", "/:companyID/personAdd/", 'companyAddPerson'),
+            self::route("POST", "/:companyID/personDelete/:personID", 'companyDeletePerson'),
+            self::route("POST", "/:companyID/eventAdd/", 'companyAddEvent'),
+            self::route("POST", "/:companyID/eventDelete/:eventID", 'companyDeleteEvent'),
+
+            self::route("POST", "/:companyID/changeName", 'companyChangeName'),
+            self::route("POST", "/:companyID/delete", 'companyDelete'),
+
             self::route("GET", "/:companyID/edit", 'companyEditPage'),
             self::route("GET", "/:companyID", 'companyPage'),
             self::route("GET", "/", 'companies'),
@@ -61,6 +71,166 @@ class CompanyController extends BaseController
         }
 
         require "app/views/companyDetails.php";
+    }
+
+    public function companyChangeName($params) {
+        $token = $this->require_authentication();
+        $id = $params['companyID'];
+        if ($id == null) {
+            $this->error404($params[0]);
+        }
+
+        $db = $this->getDBConn();
+
+        $company = Unit::fetch($db, $id);
+        if ($company == null) {
+            $this->error404($params[0]);
+        }
+
+        $company->setName($_POST['name']);
+        $company->commit($db);
+
+        $this->addFlashMessage("Changed company name to ${_POST['name']}!", self::FLASH_LEVEL_SUCCESS);
+        $this->redirect("/companies/". $company->getId() . "/edit");
+    }
+
+    public function companyDelete($params) {
+        $token = $this->require_authentication();
+        $id = $params['companyID'];
+        if ($id == null) {
+            $this->error404($params[0]);
+        }
+
+        $db = $this->getDBConn();
+
+        $res = Unit::delete($db, $id);
+
+        if($res) {
+            $this->addFlashMessage("Deleted company!", self::FLASH_LEVEL_SUCCESS);
+            $this->redirect("/companies");
+        } else {
+            $this->addFlashMessage("Unknown error when deleting company.", self::FLASH_LEVEL_SERVER_ERR);
+            $this->redirect("/companies");
+        }
+    }
+
+    public function companyAdd($params) {
+        $token = $this->require_authentication();
+
+        $db = $this->getDBConn();
+
+        $company = new Unit();
+        $company->setName("New Company");
+
+        $res = $company->commit($db);
+
+        if($res) {
+            $this->addFlashMessage("Added new company!", self::FLASH_LEVEL_SUCCESS);
+            $this->redirect("/companies/" . $company->getId() . "/edit");
+        } else {
+            $this->addFlashMessage("Unknown error when adding company.", self::FLASH_LEVEL_SERVER_ERR);
+            $this->redirect("/companies");
+        }
+    }
+
+    public function companyAddPerson($params) {
+        $token = $this->require_authentication();
+        $id = $params['companyID'];
+        if ($id == null) {
+            $this->error404($params[0]);
+        }
+
+        $db = $this->getDBConn();
+
+        $person = new Person();
+        $person->setFirstname($_POST['firstname'])
+            ->setLastname($_POST['lastname'])
+            ->setRank($_POST['rank'])
+            ->setUnitID($id);
+
+        $res = $person->commit($db);
+
+        if($res) {
+            $this->addFlashMessage("Added " . $person->getFullName(), self::FLASH_LEVEL_SUCCESS);
+        } else {
+            $this->addFlashMessage("Unknown error when adding person.", self::FLASH_LEVEL_SERVER_ERR);
+        }
+
+        $this->redirect("/companies/". $id . "/edit");
+    }
+
+    public function companyDeletePerson($params) {
+        $token = $this->require_authentication();
+        $id = $params['companyID'];
+        $personid = $params['personID'];
+
+        if ($id == null || $personid == null) {
+            $this->error404($params[0]);
+        }
+
+        $db = $this->getDBConn();
+        $res = Person::delete($db, $personid);
+
+        if($res) {
+            $this->addFlashMessage("Deleted person.", self::FLASH_LEVEL_SUCCESS);
+        } else {
+            $this->addFlashMessage("Unknown error when deleting person.", self::FLASH_LEVEL_SERVER_ERR);
+        }
+
+        $this->redirect("/companies/". $id . "/edit");
+    }
+
+    public function companyAddEvent($params) {
+        $token = $this->require_authentication();
+
+        $id = $params['companyID'];
+        if ($id == null) {
+            $this->error404($params[0]);
+        }
+
+        $db = $this->getDBConn();
+
+        $event = new UnitEvent();
+        $event->setUnitID($id)
+            ->setEvent($_POST['eventName'])
+            ->setType($_POST['type'])
+            ->setDate($_POST['date'])
+            ->setDescription($_POST['description'])
+            ->setLocationName($_POST['locationName'])
+            ->setLatitude($_POST['latitude'])
+            ->setLongitude($_POST['longitude']);
+
+        $res = $event->commit($db);
+
+        if($res) {
+            $this->addFlashMessage("Added new event!", self::FLASH_LEVEL_SUCCESS);
+        } else {
+            $this->addFlashMessage("Unknown error when adding event.", self::FLASH_LEVEL_SERVER_ERR);
+        }
+
+        $this->redirect("/companies/". $id . "/edit");
+    }
+
+    public function companyDeleteEvent($params) {
+        $token = $this->require_authentication();
+
+        $id = $params['companyID'];
+        $eventid = $params['eventID'];
+
+        if ($id == null || $eventid == null) {
+            $this->error404($params[0]);
+        }
+
+        $db = $this->getDBConn();
+        $res = UnitEvent::delete($db, $eventid);
+
+        if($res) {
+            $this->addFlashMessage("Deleted event.", self::FLASH_LEVEL_SUCCESS);
+        } else {
+            $this->addFlashMessage("Unknown error when deleting event.", self::FLASH_LEVEL_SERVER_ERR);
+        }
+
+        $this->redirect("/companies/". $id . "/edit");
     }
 
     /**
--- app/views/companyEdit.php
+++ app/views/companyEdit.php
@@ -19,13 +19,14 @@ include "app/views/_header.phtml"
     <form action="<?= $this->url('/companies/'. $company->getId()) ?>">
         <input type="submit" value="Done">
     </form>
-    <form action="<?= $this->url('/companies/'. $company->getId()) . "/delete" ?>">
+    <form method="post" action="<?= $this->url('/companies/'. $company->getId()) . "/delete" ?>" onsubmit="return confirm('Are you sure you want to delete this unit? (this cannot be undone)');">
         <input type="submit" value="Delete">
     </form>
     <p></p>
+    <h3>Change Unit Name</h3>
     <form action="<?= $this->url('/companies/'. $company->getId()) . '/changeName' ?>" method="post">
         <input type="text" name="name" value="New Unit Name">
-        <input type="submit" value="Done">
+        <input type="submit" value="Change Name">
     </form>
     <h3>Members</h3>
     <section>
@@ -43,7 +44,7 @@ include "app/views/_header.phtml"
                         <td><?= $person->getRank() ?></td>
                         <td><?= $person->getFullName() ?></td>
                         <td>
-                            <form action="<?= $this->url('/companies/' . $company->getId() . "/personDelete/" . $person->getId())?>" method="post">
+                            <form action="<?= $this->url('/companies/' . $company->getId() . "/personDelete/" . $person->getId())?>" method="post" onsubmit="return confirm('Are you sure you want to delete this person? (this cannot be undone)');">
                                 <input type="submit" value="X">
                             </form>
                         </td>
@@ -52,7 +53,7 @@ include "app/views/_header.phtml"
                 <tr><form action="<?= $this->url('/companies/' . $company->getId() . "/personAdd/") ?>" method="post">
                         <td><input type="text" name="rank" value="Rank"></td>
                         <td><input type="text" name="firstname" value="First Name"><input type="text" name="lastname" value="Last Name"></td>
-                        <td><input type="submit"></td>
+                        <td><input type="submit" value="Add"></td>
                     </form></tr>
             </tbody>
         </table>
@@ -77,14 +78,19 @@ include "app/views/_header.phtml"
                         <td><?= $event->getDescription() ?></td>
                         <td><?= $event->getLocationString() ?></td>
                         <td>
-                            <form action="<?= $this->url('/companies/' . $company->getId() . "/eventDelete/" . $event->getId())?>" method="post">
+                            <form action="<?= $this->url('/companies/' . $company->getId() . "/eventDelete/" . $event->getId())?>" method="post" onsubmit="return confirm('Are you sure you want to delete this event? (this cannot be undone)');">
                                 <input type="submit" value="X">
                             </form>
                         </td>
                     </tr>
                 <?php endforeach; ?>
                 <tr><form action="<?= $this->url('/companies/' . $company->getId() . "/eventAdd/") ?>" method="post">
-                        <td><!-- Placeholder --></td>
+                        <td><select name="type" id="" required>
+                                <option selected disabled>Select an Event Type...</option>
+                                <option value="battle">Battle</option>
+                                <option value="diary">Diary</option>
+                                <option value="event">Event</option>
+                            </select></td>
                         <td><input type="text" name="eventName" value="Name"></td>
                         <td><input type="date" name="date" value="1944-01-01"></td>
                         <td><input type="text" name="description" value="Description"></td>
@@ -93,7 +99,7 @@ include "app/views/_header.phtml"
                             <input type="number" name="latitude" value="46.8207">
                             <input type="number" name="longitude" value="2.37545">
                         </td>
-                        <td><input type="submit"></td>
+                        <td><input type="submit" value="Add"></td>
                     </form></tr>
             </tbody>
         </table>
