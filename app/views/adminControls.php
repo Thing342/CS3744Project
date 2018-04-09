08be1839f07ef04718cdc9be34c1433fd19f0632
leslie6@vt.edu <leslie6@vt.edu>
2018-04-09 15:01:45 -0400

    Adding Admin Controls to Bootstrap Version
    Admin can see all users and their level.  Working on delete and promote
    functionality.

--- app/views/adminControls.php
+++ app/views/adminControls.php
@@ -7,31 +7,34 @@ $conn = new mysqli("localhost", "root", "", "fantasticfour_p4") or die('Error: '
 ?>
 
 
-<div id = "whiteTextDiv">
+<div id = "whiteTextDiv" style="padding-left: 10px">
 
 <?php if($this->getLoggedInUser()->getType() == (int)3): ?>
-	<h2>Admin Controls</h2>
+	<h1 style="padding-top: 20px"><b>Admin Controls</b></h1>
 
 	<h3>Commenters</h3>
-	<?php
- 	$query = "SELECT * FROM `user` WHERE `type` LIKE 1";
-  	if ($result = mysqli_query($conn, $query)) {
-  		while ($row = $result->fetch_assoc()) {
-        	printf("%s", $row["username"]);
+  <div style="padding-left: 10px">
+  	<?php
+   	$query = "SELECT * FROM `user` WHERE `type` LIKE 1";
+    	if ($result = mysqli_query($conn, $query)) {
+    		while ($row = $result->fetch_assoc()) {
+          	printf("%s", $row["username"]);
 
-        	echo "<form action = \"";
-        	echo $_ENV['SUBDIRECTORY'];
-        	echo "\adminDelete\" onSubmit=\"return verification()\", method=\"post\">";
-   			echo"<input type=\"hidden\" name=\"idToDelete\" value=\"";
-   			printf("%s", $row["userID"]);
-    		echo "\"</label>";
-   			echo"<input type=\"submit\" value=\"Delete Account\">";
-    		echo "</form>";
-    		echo "<br>";
-        }
-    }
-    ?>
+          	echo "<form action = \"";
+          	echo $_ENV['SUBDIRECTORY'];
+          	echo "\adminDelete\" onSubmit=\"return verification()\", method=\"post\">";
+     			echo"<input type=\"hidden\" name=\"idToDelete\" value=\"";
+     			printf("%s", $row["userID"]);
+      		echo "\"</label>";
+     			echo"<input type=\"submit\" value=\"Delete Account\">";
+      		echo "</form>";
+      		echo "<br>";
+          }
+      }
+      ?>
+    </div>
     <h3>Editors</h3>
+    <div style="padding-left: 10px">
     <?php
  	$query = "SELECT * FROM `user` WHERE `type` LIKE 2";
   	if ($result = mysqli_query($conn, $query)) {
@@ -49,7 +52,9 @@ $conn = new mysqli("localhost", "root", "", "fantasticfour_p4") or die('Error: '
         }
     }
     ?>
+  </div>
     <h3>Admins</h3>
+    <div style="padding-left: 10px">
     <?php
  	$query = "SELECT * FROM `user` WHERE `type` LIKE 3";
   	if ($result = mysqli_query($conn, $query)) {
@@ -67,6 +72,7 @@ $conn = new mysqli("localhost", "root", "", "fantasticfour_p4") or die('Error: '
         }
     }
     ?>
+  </div>
 <?php else: ?>
     <p><i>You do not have permissions to view this page.</i></p>
 <?php endif; ?>
